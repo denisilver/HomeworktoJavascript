bab05774542718fdf1bf2e45e90167fe9670357d
Denis Sleptsov <dinis68@mail.ru>
2021-11-28 14:40:30 +0300

    1

--- Задача15/script.js
+++ Задача15/script.js
@@ -3,6 +3,6 @@ function clock(){
         hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
         minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
         seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-    document.getElementById('clock').innerHTML = 'Часы ' + hours + ':' + ' Минуты ' + minutes + ':' + ' Секунды ' + seconds;
+    document.getElementById('clock').innerHTML = 'Часы '  + ':' + hours + ' Минуты ' + ':' + minutes  + ' Секунды ' + ':' + seconds;
 }
 setInterval(clock, 1000);
